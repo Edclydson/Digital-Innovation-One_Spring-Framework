aa50c65e2541df0d1cacdac0e88eac4901eaffd8
Edclydson Sousa <edclydson.sousa@gmail.com>
2022-02-23 11:30:23 -0300

    Spring Framework - Application Context

---
+++ Introdução _Spring_Framework/Application_Context/context/src/main/java/com/app/context/repository/userRepository.java
@@ -0,0 +1,10 @@
+package com.app.context.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface userRepository {
+    /**
+     * Repositorio onde ficam os metodos mais abstratos...
+     */
+}
---
+++ Introdução _Spring_Framework/Application_Context/context/src/main/java/com/app/context/userRepository.java
@@ -0,0 +1,12 @@
+package com.app.context;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class userRepository {
+
+    public void apagadados(){
+        System.out.println("Apagando dados...");
+    }
+    
+}
